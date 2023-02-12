a8e3cc89dbc05a87359edb3fba64cd3d1be6aadd
qtchaos <72168435+qtchaos@users.noreply.github.com>
2023-02-12 22:49:26 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+    import {onMount} from "svelte";
+
+    const url = "https://router.grubby.workers.dev/get_random"
+    let products = []
+
+    onMount(async function () {
+      const response = await fetch(url);
+      products = await response.json();
+    });
+
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#if products.length === 0}
+    <p>Loading...</p>
+{:else}
+    <p>Here are some products</p>
+{/if}
+{#each products as product}
+    <div>
+        <h2>{product.name}</h2>
+        <p>{product.brand}</p>
+    </div>
+{/each}

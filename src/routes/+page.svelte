f738470a2f0a773c870597b635749cf052ec687b
qtchaos <72168435+qtchaos@users.noreply.github.com>
2023-02-13 17:37:48 +0200

    Add tailwind config.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,8 @@
 	const { products } = data;
 
 </script>
-
 <h1 class="font-monaSansBlackWide text-4xl">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="text-grayish">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 {#if products.length === 0}
 	<p>Loading...</p>
 {:else}
---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+        "lighter": "#f2f5dc",
+        "darker": "#AABA78",
+        "dark": "#c5d2a3",
+				"grayish": "#585960",
+				"superblue": "#2a303c"
+      },
+			fontFamily: {
+				monaSans: ["Mona-Sans-Regular", "sans-serif"],
+        monaSansBlack: ["Mona-Sans-Black", "sans-serif"],
+				monaSansBlackWide: ["Mona-Sans-BlackWide", "sans-serif"],
+				monaSansBoldWide: ["Mona-Sans-BoldWide", "sans-serif"],
+				monaSansExtraBoldWide: ["Mona-Sans-ExtraBoldWide", "sans-serif"],
+				monaSansMedium: ["Mona-Sans-Medium", "sans-serif"],
+				monaSansSemiBold: ["Mona-Sans-SemiBold", "sans-serif"],
+      },
+		}
+	},
+	plugins: [require('daisyui')]
+};

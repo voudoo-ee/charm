f317ea6260d90c59576adff0ec0fc3a79a520c94
qtchaos <72168435+qtchaos@users.noreply.github.com>
2023-07-19 18:06:14 +0300

    Attempt fix.

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,3 +1,9 @@
 import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL,
+        },
+    },
+});
 export default prisma;
